fb16006d2c0fbfb7158d11b939dffbcdf496f96c
Vitaliy M <vitaliy.markitanov@dh.com>
2022-11-10 12:07:38 -0500

    deleted all files

--- test/assets/project/api/helpers/securityHandlers.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  api_key: function failure(req, res, next) {
-    if (req.swagger.params.name.value === 'Scott') {
-      next();
-    } else {
-      next(new Error('no way!'));
-    }
-  }
-};
